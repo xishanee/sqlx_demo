d309849ceaf649ec5b8c47f69d5ecd397ff66f2c
Bailey Cai <bailey.cai@chemelex.com>
2025-03-27 15:48:44 -0600

    Move sqlx driver demo related files to the subfolder

--- sqlx_driver_demo/db_migrate.sh
+++ sqlx_driver_demo/db_migrate.sh
@@ -4,7 +4,7 @@
 # "postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
 export DATABASE_URL=postgresql://postgres:123456@localhost:5432/sqlx_demo_db
 
-#sqlx database create
+sqlx database create
 #sqlx migrate add db_init
 #sqlx migrate add add_config_table
 sqlx migrate run
